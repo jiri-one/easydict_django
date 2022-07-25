4a1360ccfba4a09822b47d27c71c0ba68c08f69d
jiri.one <nemec@jiri.one>
2022-07-25 21:18:51 +0200

    adjusted DB model

--- web/models.py
+++ web/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 class Record(models.Model):
-    cz = models.CharField("Word in CZech", max_length=200)
-    en = models.CharField("Word in ENglish", max_length=200)
-    notes = models.CharField("Notes about translation", max_length=200)
-    special = models.CharField("Special notes", max_length=200)
-    translator = models.CharField("Name of translator", max_length=200)
+    cze = models.CharField("Word in CZech", max_length=200)
+    eng = models.CharField("Word in ENglish", max_length=200)
+    notes_cze = models.CharField("Notes about translation",
+                             max_length=200, blank=True, null=True)
+    notes_eng = models.CharField("Notes about translation",
+                                 max_length=200, blank=True, null=True)
+    special_cze = models.CharField(
+        "Special notes", max_length=200, blank=True, null=True)
+    special_eng = models.CharField(
+        "Special notes", max_length=200, blank=True, null=True)
+    translator = models.CharField(
+        "Name of translator", max_length=200, blank=True, null=True)
+    time_added = models.DateTimeField("Date of addition", blank=True, null=True)
+    time_changed = models.DateTimeField(
+        "Date of change", blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.cz} - {self.en}' 
