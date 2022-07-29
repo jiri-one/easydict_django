cf61bec1ed9de59d3a63451d4fa9cde5df0b5ce2
jiri.one <nemec@jiri.one>
2022-07-29 15:38:25 +0200

    beginning of search function

--- web/views.py
+++ web/views.py
@@ -5,8 +5,14 @@ from django.template import loader
 
 def index_cze(request):
     template = loader.get_template('web/index_cze.html')
+    if request.method == "POST":
+        return print("test")
     return HttpResponse(template.render())
 
 def index_eng(request):
     template = loader.get_template('web/index_eng.html')
     return HttpResponse(template.render())
+
+def searchengine(request):
+    if request.method == 'POST':
+        print("test")
